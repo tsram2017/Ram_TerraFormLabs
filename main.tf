0c3a6a27cae86d9ac4e5fa8237c9044a8b93d3d9
Ramkumar <ramkumar.ts@gmail.comgit confit --global user.name Ramkumargit config --global user.email ramkumar.ts@gmail.com>
2023-06-11 00:52:52 +0530

    Main.tf_Init

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "Google" {
+  region = "us-west1"
+  zone = "us-west1-a"
+  project = "terraform-labs-388206"
+  credentials = "C:\Users\Ram\Downloads\terraform-labs-388206-8131abd3344c.json"
+}
+
+resource "google_compute_instance" "FirstVM" {
+name = "Test" 
+zone = "Asia_South1"
+
+}
+
+variable "VMtype" {
+type = string
+default = "Test"
+description = "Test"  
+}
